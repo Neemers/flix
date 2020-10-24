963184757b47237a1bc802bdfa4160e0457b24de
Rory <neemazahedi@ucsb.edu>
2020-10-23 19:10:25 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Rory Zahedi on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
